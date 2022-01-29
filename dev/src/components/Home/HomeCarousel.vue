33d9119d777239799644d5ca6f261da9abed070a
ChenMH <2036604698@qq.com>
2022-01-30 00:40:54 +0800

    feat: carousel in home page

---
+++ dev/src/components/Home/HomeCarousel.vue
@@ -0,0 +1,43 @@
+<template>
+    <el-carousel id="home-carousel" height="350px">
+        <el-carousel-item class="carousel-item">
+            <img src="@/assets/unmasked.png" alt>
+        </el-carousel-item>
+
+        <el-carousel-item class="carousel-item">
+            <img src="@/assets/holy.png" alt>
+        </el-carousel-item>
+
+        <el-carousel-item class="carousel-item">
+            <img src="@/assets/a-game.png" alt>
+        </el-carousel-item>
+    </el-carousel>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { ElCarousel, ElCarouselItem } from "element-plus";
+
+export default defineComponent({
+    name: "HomeCarousel",
+    components: {
+        ElCarousel, ElCarouselItem
+    },
+})
+</script>
+
+<style lang="scss" scoped>
+#home-carousel {
+    position: relative;
+    width: 100%;
+    height: 350px;
+
+    .carousel-item {
+        > img {
+            position: relative;
+            width: 100%;
+            opacity: 0.5;
+        }
+    }
+}
+</style>
