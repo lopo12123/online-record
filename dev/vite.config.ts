b6859389aaec00526e93574f68b0bd200747bd0d
ChenMH <2036604698@qq.com>
2022-01-29 16:25:59 +0800

    refactor: v1.0.0-beta-ok

--- dev/vite.config.ts
+++ dev/vite.config.ts
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: './',
     plugins: [vue()],
     resolve: {
         alias: {
@@ -15,7 +16,8 @@ export default defineConfig({
         port: 8080
     },
     build: {
-        outDir: '../docs'
+        outDir: '../docs',
+        emptyOutDir: true
     }
 })
 
