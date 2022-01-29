d51d39b2fdd40d036d425d3d02ab47f045ae7c77
ChenMH <2036604698@qq.com>
2022-01-29 16:17:57 +0800

    refactor: v1.0.0-beta

---
+++ docs/assets/Music.2defeae2.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.c3df9b85.js";import{d as s,c,o}from"./vendor.7ceb6368.js";const _=s({name:"Music"}),t={id:"music"};function a(n,r,d,i,p,u){return o(),c("div",t," music page ")}var v=e(_,[["render",a],["__scopeId","data-v-273ac9af"]]);export{v as default};
