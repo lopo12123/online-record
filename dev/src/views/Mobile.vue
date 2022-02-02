fa0f93ce70ed44dabeb675adab19c4daf256ac7c
ChenMH <2036604698@qq.com>
2022-02-02 20:46:04 +0800

    feat: mobile index page

---
+++ dev/src/views/Mobile.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="mobile">
+        mobile view here
+    </div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from "vue";
+
+export default defineComponent({
+    name: "Mobile"
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
