274429d48816a1d82152a3ea37a57702fe15f514
ChenMH <2036604698@qq.com>
2022-02-03 17:02:17 +0800

    feat: record overview

---
+++ dev/src/layouts/Record/RecordOverview.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="record-overview">
+        record-overview
+    </div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from "vue";
+
+export default defineComponent({
+    name: "RecordOverview"
+})
+</script>
+
+<style lang="scss" scoped>
+#record-overview {
+    position: relative;
+    width: 100%;
+    height: 100%;
+
+}
+</style>
