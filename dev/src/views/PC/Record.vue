d6bb4827339ce06ebf2e16a7a0767d4769d75fdf
ChenMH <2036604698@qq.com>
2022-02-03 16:31:57 +0800

    refactor: divide into router-view

--- dev/src/views/PC/Record.vue
+++ dev/src/views/PC/Record.vue
@@ -1,17 +1,21 @@
 <template>
     <div id="record">
-        record.vue (todo)
+        <router-view />
     </div>
 </template>
 
 <script lang="ts">
-import {defineComponent} from "vue";
+import { defineComponent } from "vue";
 
 export default defineComponent({
-    name: "Record"
+    name: "Record",
 })
 </script>
 
 <style lang="scss" scoped>
-
+#record {
+    position: relative;
+    width: 100%;
+    height: 100%;
+}
 </style>
