093f16b79538616e55fd1995b161a2527ca48980
ChenMH <2036604698@qq.com>
2022-02-04 22:57:18 +0800

    feat: provide userState

--- dev/src/App.vue
+++ dev/src/App.vue
@@ -3,3 +3,26 @@
       <router-view />
   </div>
 </template>
+
+<script lang="ts">
+import {defineComponent, provide, Ref, ref} from "vue";
+
+export type UserState = {
+    login: boolean
+    account: string
+    userToken: string
+}
+
+export default defineComponent({
+    name: 'App',
+    setup() {
+        const userState: Ref<UserState> = ref({
+            login: false,
+            account: '',
+            userToken: ''
+        })
+
+        provide<Ref<UserState>>('userState', userState)
+    }
+})
+</script>
